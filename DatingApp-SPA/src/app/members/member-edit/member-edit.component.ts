baf5a398efc43ba699f85a869c31e91bc20ff6e4
Brandon Harvie <brandon.harvie@city-holdings.co.uk>
2020-04-02 19:12:58 +0100

    end of section 10

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,9 @@ import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-member-edit',
@@ -19,7 +22,8 @@ export class MemberEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService,
+              private userService: UserService, private authService: AuthService ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -28,8 +32,11 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
+   this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
     this.alertify.success('Profile updated successfully')
     this.editForm.reset(this.user);
+   }, error => {
+     this.alertify.error(error);
+   });
   }
 }
